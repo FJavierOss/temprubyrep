46637923d4ab0699ee70f1251252cdd23b0ba8f6
FJavierOss <email_id>
2018-11-24 17:03:25 -0300

    nuevos items

---
+++ new update con schema y otros/schema-city-places.rb
@@ -0,0 +1,127 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20181121151044) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "avg_rating"
+    t.integer "country_id"
+    t.string "post_type"
+    t.bigint "post_id"
+    t.string "image"
+    t.index ["post_type", "post_id"], name: "index_cities_on_post_type_and_post_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "avg_rating"
+    t.integer "post_id"
+    t.integer "user_id"
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "headline"
+    t.text "context"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+  end
+
+  create_table "country_suscriptions", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_country_suscriptions_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_country_suscriptions_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_country_suscriptions_on_follower_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_friendships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_friendships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_friendships_on_follower_id"
+  end
+
+  create_table "itineraries", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "avg_rating"
+    t.integer "city_id"
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "avg_rating"
+    t.string "type"
+    t.integer "city_id"
+    t.integer "country_id"
+    t.string "image"
+    t.decimal "latitude"
+    t.decimal "longitude"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "avg_rating"
+    t.integer "rating"
+    t.integer "forum_id"
+    t.string "forum_type"
+    t.integer "postable_id"
+    t.string "postable_type"
+    t.integer "user_id"
+    t.integer "place_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "last"
+    t.datetime "date_of_birth"
+    t.boolean "is_female", default: false
+    t.string "country"
+    t.string "username"
+    t.integer "reputation"
+    t.string "user_type"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
